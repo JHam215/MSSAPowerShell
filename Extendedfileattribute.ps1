0b2e8f7f9cb78f57d0a5c718a500c7d9e6edae03
Jeff <43505159+JHam215@users.noreply.github.com>
2024-11-19 07:48:41 -0700

    Add files via upload

---
+++ Extendedfileattribute.ps1
@@ -0,0 +1,7 @@
+﻿$folder = (New-Object -ComObject Shell.Application).NameSpace("$pwd")
+# Note: Assumes that no indices higher than 1000 exist.
+0..1000 | % { 
+  if ($n = $folder.GetDetailsOf($null, $_)) { 
+    [pscustomobject] @{ Index = $_; Name = $n } 
+  } 
+}
---
+++ filelocation.ps1
@@ -0,0 +1,26 @@
+﻿#create a shell application
+$Shell = New-Object -ComObject Shell.application
+
+#Get file information
+$filepath = "C:\photoedit\DSC_3848.JPG"
+$folder = $shell.namespace((get-item $filepath).directoryname)
+$file = $folder.parseName((get-item $filepath).name)
+
+#$folder.GetDetailsOf($file, 21)   # Title
+#$folder.GetDetailsOf($file, 13)   # Contributing artists
+#$folder.GetDetailsOf($file, 15)   # Year
+
+#$folder.GetDetailsOf($file, f*)  
+
+# Define the path to the photo and the new author name
+$newAuthor = "Jeff Hamer"
+
+# Update the author name metadata
+Set-ItemProperty -Path $filepath -Name "Authors" -Value $newAuthor
+
+# Verify the update
+Get-ItemProperty -Path $filePath | Get-Member
+
+Get-ItemProperty -Path $filePath | FL -property * -Force
+
+
